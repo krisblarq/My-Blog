19b386929f7bd04b841af2b9725ccb599ae53d79
krisblarq <naturalicon07@gmail.com>
2021-01-28 21:59:55 +0100

    static files update

--- My_Blog_Project/settings.py
+++ My_Blog_Project/settings.py
@@ -14,6 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 TEMPLATES_DIR = os.path.join(BASE_DIR,'templates')
 STATIC_DIR = os.path.join(BASE_DIR,'static')
 MEDIA_DIR = os.path.join(BASE_DIR,'media')
